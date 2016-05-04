8f5967422e57d6516c3ada033785098ee36609d0
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-04 13:38:34 -0700

    monkey exercise draft1

---
+++ monkey_exercise.js
@@ -0,0 +1,25 @@
+
+// Create a monkey object, which has the following properties:
+
+// name
+// species
+// foodsEaten
+// And the following methods:
+
+// eatSomething(thingAsString)
+// introduce: producers a string introducing itself, including its name, species, and what it's eaten
+// Create 3 monkeys total. Make sure all 3 monkeys have all properties set and methods defined.
+
+// Exercise your monkeys by retrieving their properties and using their methods. Practice using both syntaxes for retrieving properties (dot notation and brackets).
+
+
+var monkey = [
+				name:"Nemo"
+				species:"clown fish"
+				foodsEaten:"algae"
+				// eatSomething(thingAsString);
+				// introduce: "Hi my name is " + name + " I am a " + species + " and I've eaten " + foodsEaten + ".";
+]
+
+eatSomething("");
+console.log("Hi my name is " + name + " I am a " + species + " and I've eaten " + foodsEaten + ".");
